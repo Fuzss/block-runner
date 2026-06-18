0d4a334a060aa8c78c7c7f795aa6cf489f1c55e6
Fuzss <28218241+Fuzss@users.noreply.github.com>
2026-06-18 09:05:23 +0200

    full 26.2.x port

--- Common/src/main/java/fuzs/blockrunner/common/data/ModBlockTagsProvider.java
+++ Common/src/main/java/fuzs/blockrunner/common/data/ModBlockTagsProvider.java
@@ -5,9 +5,9 @@ import fuzs.puzzleslib.common.api.data.v2.core.DataProviderContext;
 import fuzs.puzzleslib.common.api.data.v2.tags.AbstractTagProvider;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.references.BlockItemIds;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 
 public class ModBlockTagsProvider extends AbstractTagProvider<Block> {
 
@@ -21,7 +21,7 @@ public class ModBlockTagsProvider extends AbstractTagProvider<Block> {
         this.tag(ModRegistry.SLOW_BLOCKS_BLOCK_TAG);
         this.tag(ModRegistry.SLIGHTLY_SLOW_BLOCKS_BLOCK_TAG);
         this.tag(ModRegistry.SLIGHTLY_QUICK_BLOCKS_BLOCK_TAG).addTag(BlockTags.STONE_BRICKS);
-        this.tag(ModRegistry.QUICK_BLOCKS_BLOCK_TAG).add(Blocks.DIRT_PATH);
+        this.tag(ModRegistry.QUICK_BLOCKS_BLOCK_TAG).add(BlockItemIds.DIRT_PATH.block());
         this.tag(ModRegistry.VERY_QUICK_BLOCKS_BLOCK_TAG);
     }
 }
